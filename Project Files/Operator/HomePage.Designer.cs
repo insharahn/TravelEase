2ac9d53eac0391cb67737d31b07b6f992b346b71
ZaraHEREhehe <i230681@isb.nu.edu.pk>
2025-05-02 21:11:51 +0500

    Add files via upload

---
+++ Project Files/Operator/HomePage.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TourOperator
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.adminRadiobutton = new System.Windows.Forms.RadioButton();
+            this.operatorradioButton = new System.Windows.Forms.RadioButton();
+            this.travellerradioButton = new System.Windows.Forms.RadioButton();
+            this.guideradioButton = new System.Windows.Forms.RadioButton();
+            this.transportserviceradioButton = new System.Windows.Forms.RadioButton();
+            this.hotelierradioButton = new System.Windows.Forms.RadioButton();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Lucida Fax", 45F, System.Drawing.FontStyle.Bold);
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.label10.Location = new System.Drawing.Point(269, 225);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(374, 68);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "TravelEase";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Mistral", 20.75F, System.Drawing.FontStyle.Italic);
+            this.label12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.label12.Location = new System.Drawing.Point(301, 182);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(323, 34);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Ease Your Travelling Journey With";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // adminRadiobutton
+            // 
+            this.adminRadiobutton.AutoSize = true;
+            this.adminRadiobutton.BackColor = System.Drawing.Color.Transparent;
+            this.adminRadiobutton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminRadiobutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.adminRadiobutton.Location = new System.Drawing.Point(295, 306);
+            this.adminRadiobutton.Name = "adminRadiobutton";
+            this.adminRadiobutton.Size = new System.Drawing.Size(75, 31);
+            this.adminRadiobutton.TabIndex = 22;
+            this.adminRadiobutton.TabStop = true;
+            this.adminRadiobutton.Text = "Admin";
+            this.adminRadiobutton.UseVisualStyleBackColor = false;
+            this.adminRadiobutton.CheckedChanged += new System.EventHandler(this.adminRadiobutton_CheckedChanged);
+            // 
+            // operatorradioButton
+            // 
+            this.operatorradioButton.AutoSize = true;
+            this.operatorradioButton.BackColor = System.Drawing.Color.Transparent;
+            this.operatorradioButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operatorradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.operatorradioButton.Location = new System.Drawing.Point(376, 306);
+            this.operatorradioButton.Name = "operatorradioButton";
+            this.operatorradioButton.Size = new System.Drawing.Size(89, 31);
+            this.operatorradioButton.TabIndex = 23;
+            this.operatorradioButton.TabStop = true;
+            this.operatorradioButton.Text = "Operator";
+            this.operatorradioButton.UseVisualStyleBackColor = false;
+            this.operatorradioButton.CheckedChanged += new System.EventHandler(this.operatorradioButton_CheckedChanged);
+            // 
+            // travellerradioButton
+            // 
+            this.travellerradioButton.AutoSize = true;
+            this.travellerradioButton.BackColor = System.Drawing.Color.Transparent;
+            this.travellerradioButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.travellerradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.travellerradioButton.Location = new System.Drawing.Point(471, 306);
+            this.travellerradioButton.Name = "travellerradioButton";
+            this.travellerradioButton.Size = new System.Drawing.Size(88, 31);
+            this.travellerradioButton.TabIndex = 24;
+            this.travellerradioButton.TabStop = true;
+            this.travellerradioButton.Text = "Traveller";
+            this.travellerradioButton.UseVisualStyleBackColor = false;
+            this.travellerradioButton.CheckedChanged += new System.EventHandler(this.travellerradioButton_CheckedChanged);
+            // 
+            // guideradioButton
+            // 
+            this.guideradioButton.AutoSize = true;
+            this.guideradioButton.BackColor = System.Drawing.Color.Transparent;
+            this.guideradioButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guideradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.guideradioButton.Location = new System.Drawing.Point(565, 306);
+            this.guideradioButton.Name = "guideradioButton";
+            this.guideradioButton.Size = new System.Drawing.Size(68, 31);
+            this.guideradioButton.TabIndex = 25;
+            this.guideradioButton.TabStop = true;
+            this.guideradioButton.Text = "Guide";
+            this.guideradioButton.UseVisualStyleBackColor = false;
+            this.guideradioButton.CheckedChanged += new System.EventHandler(this.guideradioButton_CheckedChanged);
+            // 
+            // transportserviceradioButton
+            // 
+            this.transportserviceradioButton.AutoSize = true;
+            this.transportserviceradioButton.BackColor = System.Drawing.Color.Transparent;
+            this.transportserviceradioButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.transportserviceradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.transportserviceradioButton.Location = new System.Drawing.Point(342, 343);
+            this.transportserviceradioButton.Name = "transportserviceradioButton";
+            this.transportserviceradioButton.Size = new System.Drawing.Size(143, 31);
+            this.transportserviceradioButton.TabIndex = 26;
+            this.transportserviceradioButton.TabStop = true;
+            this.transportserviceradioButton.Text = "Transport Service";
+            this.transportserviceradioButton.UseVisualStyleBackColor = false;
+            this.transportserviceradioButton.CheckedChanged += new System.EventHandler(this.transportserviceradioButton_CheckedChanged);
+            // 
+            // hotelierradioButton
+            // 
+            this.hotelierradioButton.AutoSize = true;
+            this.hotelierradioButton.BackColor = System.Drawing.Color.Transparent;
+            this.hotelierradioButton.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hotelierradioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(206)))));
+            this.hotelierradioButton.Location = new System.Drawing.Point(491, 343);
+            this.hotelierradioButton.Name = "hotelierradioButton";
+            this.hotelierradioButton.Size = new System.Drawing.Size(83, 31);
+            this.hotelierradioButton.TabIndex = 27;
+            this.hotelierradioButton.TabStop = true;
+            this.hotelierradioButton.Text = "Hotelier";
+            this.hotelierradioButton.UseVisualStyleBackColor = false;
+            this.hotelierradioButton.CheckedChanged += new System.EventHandler(this.hotelierradioButton_CheckedChanged);
+            // 
+            // signupButton
+            // 
+            this.signupButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(192)))), ((int)(((byte)(185)))));
+            this.signupButton.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.signupButton.Location = new System.Drawing.Point(391, 402);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(123, 49);
+            this.signupButton.TabIndex = 28;
+            this.signupButton.Text = "SIGN UP";
+            this.signupButton.UseVisualStyleBackColor = false;
+            this.signupButton.Click += new System.EventHandler(this.signupButton_Click);
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(907, 543);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.hotelierradioButton);
+            this.Controls.Add(this.transportserviceradioButton);
+            this.Controls.Add(this.guideradioButton);
+            this.Controls.Add(this.travellerradioButton);
+            this.Controls.Add(this.operatorradioButton);
+            this.Controls.Add(this.adminRadiobutton);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Name = "HomePage";
+            this.Text = "HomePage";
+            this.Load += new System.EventHandler(this.HomePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.RadioButton adminRadiobutton;
+        private System.Windows.Forms.RadioButton operatorradioButton;
+        private System.Windows.Forms.RadioButton travellerradioButton;
+        private System.Windows.Forms.RadioButton guideradioButton;
+        private System.Windows.Forms.RadioButton transportserviceradioButton;
+        private System.Windows.Forms.RadioButton hotelierradioButton;
+        private System.Windows.Forms.Button signupButton;
+    }
+}
---
+++ Project Files/Operator/HomePage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TourOperator
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void HomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+        //functionality from here
+        private void adminRadiobutton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operatorradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void travellerradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guideradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void transportserviceradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hotelierradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signupButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
